cdda1c22266bf679b5e624d3d609e6ab40af0768
Monika Erdeg <merdeg@execom.eugit config --global user.email merdeg@execom.eugit config --global user.email merdeg@execom.eu>
2018-10-26 17:21:34 +0200

    Move general extensions to utils

--- Hawaii/UI/Controller/Request/NewRequestViewController.swift
+++ Hawaii/UI/Controller/Request/NewRequestViewController.swift
@@ -221,23 +221,6 @@ extension NewRequestViewController: SelectAbsenceProtocol {
 
 extension NewRequestViewController: UIScrollViewDelegate {
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
-      //  self.view.endEditing(true)
-    }
-}
-
-extension Date {
-    var yesterday: Date {
-        return Calendar.current.date(byAdding: .day, value: -1, to: noon) ?? Date()
-    }
-    var tomorrow: Date {
-        return Calendar.current.date(byAdding: .day, value: 1, to: noon) ?? Date()
-    }
-    var noon: Date {
-        return Calendar.current.date(bySettingHour: 12, minute: 0, second: 0, of: self) ?? Date()
-    }
-    
-    func convertToTimeZone(initTimeZone: TimeZone, timeZone: TimeZone) -> Date {
-        let delta = TimeInterval(timeZone.secondsFromGMT() - initTimeZone.secondsFromGMT())
-        return addingTimeInterval(delta)
+//        self.view.endEditing(true)
     }
 }
---
+++ Hawaii/Utility/Extension/DateExtension.swift
@@ -0,0 +1,26 @@
+//
+//  DateExtension.swift
+//  Hawaii
+//
+//  Created by Server on 10/26/18.
+//  Copyright © 2018 Server. All rights reserved.
+//
+
+import Foundation
+
+extension Date {
+    var yesterday: Date {
+        return Calendar.current.date(byAdding: .day, value: -1, to: noon) ?? Date()
+    }
+    var tomorrow: Date {
+        return Calendar.current.date(byAdding: .day, value: 1, to: noon) ?? Date()
+    }
+    var noon: Date {
+        return Calendar.current.date(bySettingHour: 12, minute: 0, second: 0, of: self) ?? Date()
+    }
+    
+    func convertToTimeZone(initTimeZone: TimeZone, timeZone: TimeZone) -> Date {
+        let delta = TimeInterval(timeZone.secondsFromGMT() - initTimeZone.secondsFromGMT())
+        return addingTimeInterval(delta)
+    }
+}
